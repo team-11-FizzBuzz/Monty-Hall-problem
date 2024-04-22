6611a3052cd79f716623ca37630c34614787f1a5
AjouKim <juh0825@gmail.com>
2024-04-22 18:42:52 +0900

    resolve #4

--- problem.py
+++ problem.py
@@ -1,19 +1,30 @@
-# 승리횟수 정의 win=0
-# 패배횟수 정의 lose=0
+import random
 
-# 보상저장 award = ['자동차','염소','염소']
-# 보상 리스트 섞어줌 random.shuffle(award)
+win=0 # 승리횟수
+lose=0 # 패배횟수
 
-# 리스트 중 몇번을 선택하는지 정하기 first_check = random.randint(0,2) 
-# 염소 위치 리스트에 저장 goat_location = [i for i, value in enumerate(award) if value == '염소'] 
-
-
-# 내가 선택한 위치에 염소가 있는지 확인하는 구문, 선택한 위치 = 염소위치면 다른 염소위치를 알려줌
-
-# 사회자가 알려줄 염소위치를 변수에 저장한다
+award = ['자동차','염소','염소'] #보상저장
+random.shuffle(award) # 보상 리스트 섞어줌
 
 
+for j in range(1000):
+    first_check = random.randint(0,2) # 리스트 중 몇번을 선택하는지 정하기
+    goat_location = [i for i, value in enumerate(award) if value == '염소'] #염소 위치 리스트에 저장
 
-# 내가 처음에 선택한 위치와 알려준 염소위치의 리스트
 
-# total_list에 없는걸 선택하는게 결국 선택위치를 바꾸는게 됨
+    if(goat_location[0]!=first_check): #내가 선택한 위치에 염소가 있는지 확인하는 구문, 선택한 위치 = 염소위치면 다른 염소위치를 알려줌
+        presentation = goat_location[0] #사회자가 알려줄 염소위치를 변수에 저장한다
+    else:
+        presentation = goat_location[1]
+
+
+    total_list = [presentation,first_check] #내가 처음에 선택한 위치와 알려준 염소위치의 리스트
+
+    for i in range(3):# total_list에 없는걸 선택하는게 결국 선택위치를 바꾸는게 됨
+        if i not in total_list:
+            if award[i] == '자동차':
+                win+=1
+                break
+            else:
+                lose+=1
+                break
